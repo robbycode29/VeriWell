40b51ae44ff60122f62b6a2d17d05e343bf7f62b
Robert Oprescu <robert.oprescu_ext@sonepar.com>
2025-01-29 13:45:26 +0200

    restructuring

--- veriwell_backend/core/api/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import DefaultRouter
-
-from core.api.viewsets import InfluencersViewSet, ClaimsViewSet
-
-router = DefaultRouter()
-router.register(r'influencers', InfluencersViewSet, basename='influencers')
-router.register(r'claims', ClaimsViewSet, basename='claims')
