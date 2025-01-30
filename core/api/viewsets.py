91d392020c9678543b4d34b69b39130faf281aa9
Robert Oprescu <robert.oprescu_ext@sonepar.com>
2025-01-30 19:07:23 +0200

    adding retrieve on claim

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -402,3 +402,6 @@ class ClaimResearchViewSet(viewsets.ModelViewSet):
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
 
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
